36a106ef44429124ed2c55bbd000e99e25a96653
HtR212 <76406651+HtR212@users.noreply.github.com>
2022-05-22 20:45:57 -0400

    Delete venv directory

--- venv/bin/mm-send-comm.py
+++
@@ -1,9 +0,0 @@
-#!/home/student/Frank/OpenAPS-Glucosym-3.9/venv/bin/python
-# PYTHON_ARGCOMPLETE_OK
-
-from decocare.helpers import messages
-
-if __name__ == '__main__':
-  app = messages.SendMsgApp( )
-  app.run(None)
-
